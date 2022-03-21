0ad505c1789d8fc7b3a2f5d80a8a61ce1d235182
Wodey <54207104+Wodey@users.noreply.github.com>
2022-03-21 12:29:14 +0300

    now it can load_playlists

--- main.py
+++ main.py
@@ -10,9 +10,19 @@ load_dotenv()
 
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
 
-for i in tqdm(range(5565//20)):
-    tracks = sp.current_user_saved_tracks(limit=20, offset=(i * 20))['items']
-    with open('mymusic.txt', 'a', encoding='utf-8') as file:
-        for song in tqdm(tracks):
-            file.write(f"{song['track']['name']} - {song['track']['artists'][0]['name']} in album: {song['track']['album']['name']} \n")
+def load_favourite_tracks():
+    for i in tqdm(range(5565//20)):
+        tracks = sp.current_user_saved_tracks(limit=20, offset=(i * 20))['items']
+        with open('mymusic.txt', 'a', encoding='utf-8') as file:
+            for song in tqdm(tracks):
+                file.write(f"{song['track']['name']} - {song['track']['artists'][0]['name']} in album: {song['track']['album']['name']} \n")
 
+def load_playlists():
+    for k in tqdm(sp.current_user_playlists()['items']):
+        # print(k['tracks'])
+        playlist = sp.playlist(k['href'])
+        with open(f"{playlist['name']}.txt", 'a', encoding='utf-8') as file:
+            for song in tqdm(playlist['tracks']['items']):
+                file.write(f"{song['track']['name']} - {song['track']['artists'][0]['name']} in album: {song['track']['album']['name']} \n")
+
+load_playlists()
